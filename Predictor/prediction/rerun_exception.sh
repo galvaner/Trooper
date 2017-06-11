30f962c9b0174d32aac20dcc2307d56d6b8899bc
galvaner <r.galvanek@outlook.sk>
2017-06-11 23:18:54 +0200

    rerun_exception.sh modified

--- Predictor/prediction/rerun_exception.sh
+++ Predictor/prediction/rerun_exception.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+# reruns all FARFAR predictionwhich ended with core dump (not exception because it could be walltime exceeded)
 start_dir=`pwd` 
+echo "++Start rerun exceptions++"
 for F in `find -L ./*/*/*/*/core -type f`
 do
 	#echo $F
 	dir=`echo "$F" | cut -d "/" -f1-5`
 	cd "$dir"
+	currentDir=`pwd`
+	echo "	Currently rerunning: ${currentDir}"
 	rm ./start_script.sh.e*
 	rm ./start_script.sh.o*
 	rm ./core
-	qsub start_script.sh -l nodes=1:ppn=6,mem=16gb,walltime=23h
+	qsub -l select=1:ncpus=4:mem=16gb -l walltime=23:59:00 start_script.sh
 	cd "$start_dir"
 done
