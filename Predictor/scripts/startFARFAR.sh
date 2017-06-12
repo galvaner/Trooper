6110672ea4c397be9b26967f5ba68fc1cb7cfc54
galvaner <r.galvanek@outlook.sk>
2017-06-12 17:14:38 +0200

    startFARFAR.sh modified

--- Predictor/scripts/startFARFAR.sh
+++ Predictor/scripts/startFARFAR.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+
+# start prediction of all prepared target-template pairs
+
 # install tools
 export ROSETTA='/storage/praha1/home/galvaner/rosetta/rosetta_bin_linux_2015.38.58158_bundle/'
 export PATH=$ROSETTA/tools/rna_tools/bin/:$PATH
 source $ROSETTA/tools/rna_tools/INSTALL
 
-# create directories in prediction folder and copy all the files (including script /main_folder/prediction/start_prediction.sh)
+# create directories in prediction folder and copy all the files (including script /Predictor/prediction/start_prediction.sh)
 cd ../prediction
-for filename in ./*/*/files/*/*.pdb; do #*
+
+# copy prepared parts of .pdb files to newly created rosetta folder
+
+for filename in ./*/*/files/*/*.pdb; do 
 	predictionDirname1=$(echo $filename | cut -f 2 -d '/')
 	predictionDirname2=$(echo $filename | cut -f 3 -d '/')
 	cd $predictionDirname1/$predictionDirname2
@@ -21,6 +27,8 @@ for filename in ./*/*/files/*/*.pdb; do #*
 	cd ../../..
 done
 
+# copy prepared parts of preparedStatements.txt files to rosetta folder
+
 for filename in ./*/*/files/*.txt; do
 	predictionDirname1=$(echo $filename | cut -f 2 -d '/')
 	predictionDirname2=$(echo $filename | cut -f 3 -d '/')
@@ -28,6 +36,8 @@ for filename in ./*/*/files/*.txt; do
 	cp ../../../$filename .
 	cd ../../..
 done
+
+# copy target fasta file to rosetta folder
 
 for filename in ./*/*/files/*.fasta; do
 	predictionDirname1=$(echo $filename | cut -f 2 -d '/')
@@ -37,14 +47,15 @@ for filename in ./*/*/files/*.fasta; do
 	cd ../../..
 done
 
+# run start_prediction.sh for each target-template pair
+
 for D in `find ./?*/?*/rosetta -maxdepth 0 -mindepth 0 -type d`
 do
 	cd "$D"
 	pwd
 	#result=${PWD##*/}
 	#echo $result
-	echo "++++++++start_prediction.sh+++++++++"
+	echo "++Run start_prediction.sh++"
 	./start_prediction.sh
-	echo "--------start_prediction.sh---------"
 	cd ../../..
 done
